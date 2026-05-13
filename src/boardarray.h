6a41e0e8e1c950384a46509b9f4844a1fd236596
LAB13_USER ON WS02 <L13X11W02@LabHCI.net>
2026-05-13 11:42:55 +0800

    updated boardarray and project setup

--- src/boardarray.h
+++ src/boardarray.h
@@ -18,6 +18,23 @@ class BoardArray : public Board {
             // ALGORITHM IS PROVIDED IN INSTRUCTIONS.TXT
         }
 
+        void add(Entry* entry) {
+            if (index >= SIZE) {
+                cout << "Board is full!" << endl;
+                return;
+            }
+
+            array[index] = *entry;
+
+            int i = index;
+            while (i > 0 && array[i].compare(&array[i - 1])) {
+                swap(array[i], array[i - 1]);
+                i--;
+            }
+
+    index++;
+}
+
         void print() {
             for (int i = 0; i < index; i++) {
                 cout << i + 1 << ". ";
